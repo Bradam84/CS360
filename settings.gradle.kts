85dd1efd39306e15207c8260bec500bcc414d95a
Bradam84 <80084901+Bradam84@users.noreply.github.com>
2023-12-11 17:17:25 -0500

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Bradley Goerke CS360 Project"
+include(":app")
